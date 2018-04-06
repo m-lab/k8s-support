f03c898008d7ce697d0f02fcc927d82eb619744f
Peter Boothe <pboothe@google.com>
2018-04-06 17:42:59 -0400

    Indenting and commenting

--- cloud/README.md
+++ cloud/README.md
@@ -12,7 +12,10 @@ Every Node in a k8s system is allocated its own /24. We have 500+ nodes already,
 and through platform expansion that number could at least quadruple. So we need
 a subnet that supports more than 2000 /24 address blocks. Doing the math (`32 -
 log_2(256 ips per node * 2000 nodes on the platform) = 12`) it looks like we
-need a `/12` block.  The `10.0.0.0/8` block is used by Google cloud, so we either need to fight their configs or use one of the others.  `192.168.0.0/16` is too small, so that leaves the little-known `172.16.0.0/12` block for the M-Lab platform.
+need a `/12` block.  The `10.0.0.0/8` block is used by Google cloud, so we
+either need to fight their configs or use one of the others.  `192.168.0.0/16`
+is too small, so that leaves the little-known `172.16.0.0/12` block for the
+M-Lab platform.
 
 We will use Calico to set things up. It uses standard pieces, and has a flexible
 config language that allows us to provide rich options and/or specified external
--- cloud/setup_cloud_k8s_master.sh
+++ cloud/setup_cloud_k8s_master.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
+#
+# setup_cloud_k8s_master.sh sets up the master node in GCE for k8s in the cloud.
+#
+# By default (with no special environment variables set) it will set up in
+# mlab-sandbox.  To change the defaults, override the shell variables
+# ${GOOGLE_CLOUD_PROJECT}, ${GOOGLE_CLOUD_ZONE}, and ${K8S_GCE_MASTER}.
+#
+# This script is intended to eventually be run from Travis, but right now
+# requires a human.  Be careful with it, as it changes your default gcloud
+# project and zone.
 
 set -euxo pipefail
 
 PROJECT=${GOOGLE_CLOUD_PROJECT:-mlab-sandbox}
-REGION=${GOOGLE_CLOUD_REGION:-us-central}
-ZONE=${GOOGLE_CLOUD_REGION:-us-central1-c}
+ZONE=${GOOGLE_CLOUD_ZONE:-us-central1-c}
 GCE_NAME=${K8S_GCE_MASTER:-k8s-platform-master}
 
 # Add gcloud to PATH.
 source "${HOME}/google-cloud-sdk/path.bash.inc"
 
-## Add m-lab/travis help lib
-#source "$TRAVIS_BUILD_DIR/travis/gcloudlib.sh"
-
-# Set the project and zone for all future gcloud commands.
+# Set the project and zone for all future gcloud commands.  This alters the
+# surrounding environment, which is okay in Travis and more questionable in a
+# shell context.
 gcloud config set project $PROJECT
 gcloud config set compute/zone $ZONE
 
@@ -24,14 +32,20 @@ fi
 
 # Create the new GCE instance.
 gcloud compute instances create $GCE_NAME \
-  --image "ubuntu-1604-xenial-v20180323" --image-project "ubuntu-os-cloud" --boot-disk-size "10" --boot-disk-type "pd-standard" --boot-disk-device-name $GCE_NAME  \
+  --image "ubuntu-1604-xenial-v20180323" \
+  --image-project "ubuntu-os-cloud" \
+  --boot-disk-size "10" \
+  --boot-disk-type "pd-standard" \
+  --boot-disk-device-name $GCE_NAME  \
   --tags "dmz" \
   --machine-type "n1-standard-2"
 
 #  Give the instance time to appear.  Make sure it appears twice - there have
 #  been multiple instances of it connecting just once and then failing again for
 #  a bit.
-until gcloud compute ssh $GCE_NAME --command true && gcloud compute ssh $GCE_NAME --command true; do
+until gcloud compute ssh $GCE_NAME --command true && \
+      sleep 10 && \
+      gcloud compute ssh $GCE_NAME --command true; do
   echo Waiting for $GCE_NAME to boot up
   # Refresh keys in case they changed mid-boot. They change as part of the
   # GCE bootup process, and it is possible to ssh at the precise moment a
@@ -47,22 +61,22 @@ done
 # Kubeadm-installer has some jank, and seems to leave the shell in a bad state,
 # or possibly there is a race condition.  For whatever reason, all problems go
 # away if you disconnect and reconnect after running that command.
-gcloud compute ssh $GCE_NAME <<EOF
-sudo -s
-apt-get update -y
-apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-echo deb http://apt.kubernetes.io/ kubernetes-xenial main | tee /etc/apt/sources.list.d/kubernetes.list
-apt-get update -y
-apt-get install -y docker.io
-systemctl daemon-reload
-systemctl enable docker
-systemctl restart docker
-docker run -i \
-  -v /etc:/rootfs/etc \
-  -v /opt:/rootfs/opt \
-  -v /usr/bin:/rootfs/usr/bin \
-  xakra/kubeadm-installer:0.4.5 ubuntu install || true
+gcloud compute ssh $GCE_NAME <<-EOF
+  sudo -s
+  apt-get update -y
+  apt-get install -y apt-transport-https
+  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+  echo deb http://apt.kubernetes.io/ kubernetes-xenial main | tee /etc/apt/sources.list.d/kubernetes.list
+  apt-get update -y
+  apt-get install -y docker.io
+  systemctl daemon-reload
+  systemctl enable docker
+  systemctl restart docker
+  docker run -i \
+    -v /etc:/rootfs/etc \
+    -v /opt:/rootfs/opt \
+    -v /usr/bin:/rootfs/usr/bin \
+    xakra/kubeadm-installer:0.4.7 ubuntu install || true
 EOF
 
 # Find the instance's external IP
@@ -70,20 +84,20 @@ EXTERNAL_IP=$(gcloud compute instances list --filter "name=${GCE_NAME}" \
                 --format flattened | grep natIP | awk '{print $2}')
 
 # Become root and start everything
-gcloud compute ssh $GCE_NAME <<EOF
-sudo -s
-systemctl daemon-reload
-systemctl enable docker kubelet
-systemctl restart docker kubelet
-kubeadm init --apiserver-advertise-address ${EXTERNAL_IP}
+gcloud compute ssh $GCE_NAME <<-EOF
+  sudo -s
+  systemctl daemon-reload
+  systemctl enable docker kubelet
+  systemctl restart docker kubelet
+  kubeadm init --apiserver-advertise-address ${EXTERNAL_IP}
 EOF
 
 # Allow the user who installed k8s on the master to call kubectl.  As we
 # productionize this process, this code should be deleted.
 # For the next steps, we no longer want to be root.
-gcloud compute ssh $GCE_NAME <<EOF
-set -x
-mkdir -p \$HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf \$HOME/.kube/config
-sudo chown \$(id -u):\$(id -g) \$HOME/.kube/config
+gcloud compute ssh $GCE_NAME <<-EOF
+  set -x
+  mkdir -p \$HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf \$HOME/.kube/config
+  sudo chown \$(id -u):\$(id -g) \$HOME/.kube/config
 EOF
