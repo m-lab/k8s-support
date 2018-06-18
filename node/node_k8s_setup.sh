b2b596354684ef26b43aed5a546b13fb0759bb17
Peter Boothe <pboothe@google.com>
2018-06-18 16:01:43 -0400

    Better use of ""

--- node/node_k8s_setup.sh
+++ node/node_k8s_setup.sh
@@ -27,7 +27,7 @@ METRO="${SITE/[0-9]*/}"
 
 # Make sure to download any and all necessary auth tokens prior to this point.
 # It should be a simple wget from the master node to make that happen.
-MASTER_NODE=k8s-platform-master."${GCP_PROJECT}".measurementlab.net
+MASTER_NODE="k8s-platform-master.${GCP_PROJECT}.measurementlab.net"
 
 # TODO(https://github.com/m-lab/k8s-support/issues/29) This installation of
 # things into etc should be done as part of cloud-config.yml or ignition or just
@@ -49,14 +49,14 @@ pushd /opt/cni/bin
   # TODO: Build index_to_ip into the epoxy image
   # TODO: Use index2ip built in go rather than the horrible index_to_ip shell
   # script.
-  wget https://storage.googleapis.com/k8s-platform-"${GCP_PROJECT}"/bin/index_to_ip
+  wget "https://storage.googleapis.com/k8s-platform-${GCP_PROJECT}/bin/index_to_ip"
   chmod +x index_to_ip
 popd
 
 # Make all the shims so that network plugins can be debugged
 mkdir -p /opt/shimcni/bin
 pushd /opt/shimcni/bin
-  wget https://storage.googleapis.com/k8s-platform-"${GCP_PROJECT}"/bin/shim.sh
+  wget "https://storage.googleapis.com/k8s-platform-${GCP_PROJECT}/bin/shim.sh"
   chmod +x shim.sh
   for i in /opt/cni/bin/*; do
     ln -s /opt/shimcni/bin/shim.sh /opt/shimcni/bin/$(basename "$i")
