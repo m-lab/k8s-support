f9d8dcd8cf8e019626d94e528b9a47c5e97318df
Nathan Kinkade <nath@nkinka.de>
2019-01-25 17:02:47 -0700

    Changes order of operations from add-kubectl-del to del-add-kubectl.

--- monitor_scaling.sh
+++ monitor_scaling.sh
@@ -16,50 +16,14 @@
 # cluster in the same directory as this script.
 KUBECTL_ARGS="--kubeconfig ./kube-config"
 
+# The node on which to run the kubectl-exec command.
+KUBECTL_EXEC_NODE="mlab4.bru02"
+
 # Get the count of mlab4 nodes. Each of these nodes should have a pod scheduled
 # on them, so the running number of pods for a given daemonset should match this
 # number once that daemonset is fully deployed.
 MLAB4_COUNT=$(kubectl $KUBECTL_ARGS get nodes | grep mlab4 | wc -l)
 echo "mlab4 count: ${MLAB4_COUNT}"
-
-#
-# Create the ndt Daemonset
-#
-# Reset the built-in bash variable SECONDS.
-SECONDS=0
-
-echo -n "Created ndt DaemonSet in: "
-kubectl $KUBECTL_ARGS apply -f \
-    ./k8s/daemonsets/experiments/ndt.yml > /dev/null
-APPLY_CMD_SECS="${SECONDS}"
-echo "${APPLY_CMD_SECS}"
-
-echo -n "All pods in Running state: "
-while true; do
-  POD_COUNT=$(kubectl $KUBECTL_ARGS get pods | grep ndt | \
-      grep Running | wc -l)
-  if [[ "${POD_COUNT}" -eq "${MLAB4_COUNT}" ]]; then
-    break
-  fi
-  # Loosen the loop just a tiny bit.
-  sleep 1
-done
-APPLY_RUNNING_SECS="${SECONDS}"
-echo "${APPLY_RUNNING_SECS}"
-
-#
-# Time basic kubectl-exec command to a random ndt-server pod.
-#
-RANDOM_NDT_POD=$(kubectl $KUBECTL_ARGS get --no-headers=true pods \
-    -o custom-columns=:metadata.name | grep ndt | shuf | head -n1)
-
-# Reset the built-in bash variable SECONDS.
-SECONDS=0
-
-echo -n "Ran kubectl-exec on ndt pod in: "
-kubectl $KUBECTL_ARGS exec $RANDOM_NDT_POD -c ndt-server /bin/ls > /dev/null
-KUBECTL_EXEC_SECS="${SECONDS}"
-echo "${KUBECTL_EXEC_SECS}"
 
 #
 # Delete the ndt-server DaemonSet.
@@ -84,5 +48,47 @@ done
 DEL_DONE_SECS="${SECONDS}"
 echo "${DEL_DONE_SECS}"
 
-echo -e "\nmlab4_cnt,apply_cmd,apply_running,kubectl_exec,delete_cmd,delete_done"
-echo "${MLAB4_COUNT},${APPLY_CMD_SECS},${APPLY_RUNNING_SECS},${KUBECTL_EXEC_SECS},${DEL_CMD_SECS},${DEL_DONE_SECS}"
+
+#
+# Create the ndt Daemonset
+#
+# Reset the built-in bash variable SECONDS.
+SECONDS=0
+
+echo -n "Created ndt DaemonSet in: "
+kubectl $KUBECTL_ARGS apply -f \
+    ./k8s/daemonsets/experiments/ndt.yml > /dev/null
+APPLY_CMD_SECS="${SECONDS}"
+echo "${APPLY_CMD_SECS}"
+
+echo -n "All pods in Running state in: "
+while true; do
+  POD_COUNT=$(kubectl $KUBECTL_ARGS get pods | grep ndt | \
+      grep Running | wc -l)
+  if [[ "${POD_COUNT}" -eq "${MLAB4_COUNT}" ]]; then
+    break
+  fi
+  # Loosen the loop just a tiny bit.
+  sleep 1
+done
+APPLY_RUNNING_SECS="${SECONDS}"
+echo "${APPLY_RUNNING_SECS}"
+
+#
+# Time basic kubectl-exec command to a specific ndt pod.
+#
+KUBECTL_EXEC_POD=$(kubectl $KUBECTL_ARGS get pods -o wide | grep ndt | \
+    grep $KUBECTL_EXEC_NODE | awk '{print $1}')
+
+# Reset the built-in bash variable SECONDS.
+SECONDS=0
+
+echo -n "Ran kubectl-exec on ndt pod on ${KUBECTL_EXEC_NODE} in: "
+kubectl $KUBECTL_ARGS exec $KUBECTL_EXEC_POD -c ndt-server /bin/ls > /dev/null
+KUBECTL_EXEC_SECS="${SECONDS}"
+echo "${KUBECTL_EXEC_SECS}"
+
+echo -e "\nmlab4_cnt, delete_cmd, delete_done, apply_cmd, apply_running," \
+        "kubectl_exec"
+echo "${MLAB4_COUNT}, ${DEL_CMD_SECS}, ${DEL_DONE_SECS}, ${APPLY_CMD_SECS}," \
+     "${APPLY_RUNNING_SECS}, ${KUBECTL_EXEC_SECS}"
