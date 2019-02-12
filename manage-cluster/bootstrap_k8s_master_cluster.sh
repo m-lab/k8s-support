6ee17efa30e315f1dfdfa4ec55af85cec4397169
Nathan Kinkade <nath@nkinka.de>
2019-02-12 10:01:44 -0700

    Updates the etcdctl environment variables in root's .bashrc file to use version 3 and v3 names.

--- manage-cluster/bootstrap_k8s_master_cluster.sh
+++ manage-cluster/bootstrap_k8s_master_cluster.sh
@@ -811,10 +811,11 @@ EOF
     sudo cp -i /etc/kubernetes/admin.conf /root/.kube/config
     sudo chown $(id -u root):$(id -g root) /root/.kube/config
     sudo bash -c "(cat <<-EOF2
+        export ETCDCTL_API=3
 		export ETCDCTL_DIAL_TIMEOUT=3s
-		export ETCDCTL_CA_FILE=/etc/kubernetes/pki/etcd/ca.crt
-		export ETCDCTL_CERT_FILE=/etc/kubernetes/pki/etcd/peer.crt
-		export ETCDCTL_KEY_FILE=/etc/kubernetes/pki/etcd/peer.key
+		export ETCDCTL_CACERT=/etc/kubernetes/pki/etcd/ca.crt
+		export ETCDCTL_CERT=/etc/kubernetes/pki/etcd/peer.crt
+		export ETCDCTL_KEY=/etc/kubernetes/pki/etcd/peer.key
 		export ETCDCTL_ENDPOINTS=https://127.0.0.1:2379
 EOF2
 	) >> /root/.bashrc"
