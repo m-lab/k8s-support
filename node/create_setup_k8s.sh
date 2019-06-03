ae1b076e34b506aaf3d6cd4ab03d145b15b34f5f
Nathan Kinkade <kinkade@measurementlab.net>
2019-06-03 15:20:27 -0600

    Adds a tiny script to evaluate the setup_k8s.sh template.

---
+++ node/create_setup_k8s.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# A small script meant to be run with an alpine Docker image in Google Cloud
+# Build which determines the hash of the k8s platform cluster CA cert, and then
+# uses that to evaluate the setup_k8s.sh template.
+
+set -euxo pipefail
+
+apk add openssl
+
+# Generate a hash of the CA cert.
+# https://kubernetes.io/docs/reference/setup-tools/kubeadm/kubeadm-join/#token-based-discovery-with-ca-pinning
+ca_cert_hash=$(openssl x509 -pubkey -in ./ca.crt | \
+    openssl rsa -pubin -outform der 2>/dev/null | \
+    openssl dgst -sha256 -hex | sed 's/^.* //')
+
+sed -e "s/{{CA_CERT_HASH}}/${ca_cert_hash}/" ../node/setup_k8s.sh.template \
+    > ./setup_k8s.sh
