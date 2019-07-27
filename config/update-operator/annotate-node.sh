0918efbd3813a7c0929a87b68caeb618875dc6bd
Nathan Kinkade <kinkade@measurementlab.net>
2019-07-27 12:21:26 -0600

    Wraps curl header fields in single quotes.

--- config/update-operator/annotate-node.sh
+++ config/update-operator/annotate-node.sh
@@ -14,8 +14,8 @@ apk add curl
 # Set the annotation via the REST API.
 # Note: $UPDATE_AGENT_NODE is passed in via an environment variable.
 curl -k -X PATCH \
-    -H "Accept: application/json" \
-    -H "Authorization: Bearer $KUBE_TOKEN" \
-    -H "Content-Type: application/merge-patch+json" \
+    -H "'Accept: application/json'" \
+    -H "'Authorization: Bearer $KUBE_TOKEN'" \
+    -H "'Content-Type: application/merge-patch+json'" \
     -d "'{\"metadata\":{\"annotations\":{\"${ANNOTATION}\":\"true\"}}}'" \
     "${API_URL}/${UPDATE_AGENT_NODE}" \
