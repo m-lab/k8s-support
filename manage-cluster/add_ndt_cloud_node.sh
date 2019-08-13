7dbd67bd7dbe727d6da348361b2d751057ffe7b2
Nathan Kinkade <kinkade@measurementlab.net>
2019-08-12 20:44:15 -0600

    Adds new parameter CLOUD_ZONE so that we can deploy ndtcloud instances in any zone we want. Also creates a kubernetes subnet in that zone, if it doesn't already exist.

--- manage-cluster/add_ndt_cloud_node.sh
+++ manage-cluster/add_ndt_cloud_node.sh
@@ -10,7 +10,13 @@ set -euxo pipefail
 USAGE="USAGE: $0 <cloud-project> <cloud-site>"
 PROJECT=${1:? Please specify a GCP project: ${USAGE}}
 CLOUD_SITE=${2:? Please specify a cloud site (ending in 'c'): ${USAGE}}
+CLOUD_ZONE=${3:? Please specify the GCP zone for this VM: ${USAGE}}
+
 SITE_REGEX="[a-z]{3}[0-9]c"
+
+# Source configuration variables and bootstrap functions.
+source k8s_deploy.conf
+source bootstraplib.sh
 
 # Don't proceed if the site name doesn't match a standard cloud site name.
 if ! [[ "${CLOUD_SITE}" =~ $SITE_REGEX ]]; then
@@ -18,22 +24,9 @@ if ! [[ "${CLOUD_SITE}" =~ $SITE_REGEX ]]; then
   exit 1
 fi
 
-# Source all of the global configuration variables.
-source k8s_deploy.conf
-
-# Create a string representing region and zone variable names for this project.
-GCE_REGION_VAR="GCE_REGION_${PROJECT//-/_}"
-GCE_ZONES_VAR="GCE_ZONES_${PROJECT//-/_}"
-
-# Dereference the region and zones variables.
-GCE_REGION="${!GCE_REGION_VAR}"
-GCE_ZONES="${!GCE_ZONES_VAR}"
-
-# Grab the first zone in the list of GCE_ZONES.
-GCE_ZONE="${GCE_REGION}-$(echo ${GCE_ZONES} | awk '{print $1}')"
-
+# Determine the region based on $CLOUD_ZONE.
+GCE_REGION="${CLOUD_ZONE%-*}"
 GCP_ARGS=("--project=${PROJECT}" "--quiet")
-GCE_ARGS=("--zone=${GCE_ZONE}" "${GCP_ARGS[@]}")
 
 GCE_NAME="mlab1-${CLOUD_SITE}-measurement-lab-org"
 K8S_NAME="mlab1.${CLOUD_SITE}.measurement-lab.org"
@@ -55,7 +48,22 @@ else
       "${GCP_ARGS[@]}")
 fi
 
-./add_k8s_cloud_node.sh -p "${PROJECT}" -n "${GCE_NAME}" -h "${K8S_NAME}" \
-    -a "${GCE_NAME}" -t "ndt-cloud" \
+# If a subnet for the region of this VM doesn't already exist, then create it.
+EXISTING_SUBNET=$(gcloud compute networks subnets list \
+    --filter "name=${GCE_K8S_SUBNET} AND region:( ${GCE_REGION} )" \
+    --network "${GCE_NETWORK}" \
+    --format "value(name)" \
+    "${GCP_ARGS[@]}" || true)
+if [[ -z "${EXISTING_SUBNET}" ]]; then
+  N=$( find_lowest_network_number )
+  gcloud compute networks subnets create "${GCE_K8S_SUBNET}" \
+      --network "${GCE_NETWORK}" \
+      --range "10.${N}.0.0/16" \
+      --region "${GCE_REGION}" \
+      "${GCP_ARGS[@]}"
+fi
+
+./add_k8s_cloud_node.sh -p "${PROJECT}" -z "${CLOUD_ZONE}" \
+    -n "${GCE_NAME}" -h "${K8S_NAME}" -a "${GCE_NAME}" -t "ndt-cloud" \
     -l "mlab/type=cloud mlab/machine=mlab1 mlab/metro=${CLOUD_SITE::-2} mlab/site=${CLOUD_SITE} mlab/project=${PROJECT}"
 
