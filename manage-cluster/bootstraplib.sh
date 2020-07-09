187bfef22e09372151688ef7c6d3114044eecf1b
Nathan Kinkade <kinkade@measurementlab.net>
2020-07-08 20:07:56 -0600

    Uses sed to modify the list of --listen-metrics-urls for etcd.

--- manage-cluster/bootstraplib.sh
+++ manage-cluster/bootstraplib.sh
@@ -291,14 +291,6 @@ EOF
 
       kubeadm init --config kubeadm-config.yml
 
-      # Modify the --advertise-address flag to point to the external IP,
-      # instead of the internal one that kubeadm populated. This is necessary
-      # because external nodes (and especially kube-proxy) need to know of the
-      # master node by its public IP, even though it is technically running in
-      # a private VPC.
-      sed -i -re 's|(advertise-address)=.+|\1=${EXTERNAL_IP}|' \
-          /etc/kubernetes/manifests/kube-apiserver.yaml
-
       # Copy the admin KUBECONFIG file to the GCS bucket.
       cp /etc/kubernetes/admin.conf ${K8S_PKI_DIR}
 
@@ -333,16 +325,39 @@ EOF
 
       # Join the master node to the existing cluster.
       kubeadm join --config kubeadm-config.yml
-
-      # Modify the --advertise-address flag to point to the external IP,
-      # instead of the internal one that kubeadm populated. This is necessary
-      # because external nodes (and especially kube-proxy) need to know of the
-      # master node by its public IP, even though it is technically running in
-      # a private VPC.
-      sed -i -re 's|(advertise-address)=.+|\1=${EXTERNAL_IP}|' \
-          /etc/kubernetes/manifests/kube-apiserver.yaml
 EOF
   fi
+
+  # Modify a few of the generated static manifests to suit our needs.
+  gcloud compute ssh "${gce_name}" "${GCE_ARGS[@]}" <<EOF
+    set -euxo pipefail
+    sudo --login
+
+    # Bash options are not inherited by subshells. Reset them to exit on any error.
+    set -euxo pipefail
+
+    # Modify the --advertise-address flag to point to the external IP,
+    # instead of the internal one that kubeadm populated. This is necessary
+    # because external nodes (and especially kube-proxy) need to know of the
+    # master node by its public IP, even though it is technically running in
+    # a private VPC.
+    sed -i -re 's|(advertise-address)=.+|\1=${EXTERNAL_IP}|' \
+        /etc/kubernetes/manifests/kube-apiserver.yaml
+
+    # Modify the default --listen-metrics-urls flag to listen on the VPC internal
+    # IP address (the default is localhost). Sadly, this cannot currently be
+    # defined in the configuration file, since the only place to define etcd
+    # extraArgs is in the ClusterConfiguration, which applies to the entire
+    # cluster, not a single etcd instances in a cluster.
+    # https://github.com/kubernetes/kubeadm/issues/2036
+    sed -i -re '/listen-metrics-urls/ s|$|,http://${INTERNAL_IP}:2381|' \
+        /etc/kubernetes/manifests/etcd.yaml
+
+    # The above modifications to manifests will cause the api-server and etcd
+    # to be restarted by the kubelet. Stop and wait here for a little bit to
+    # give them time to restart before we continue.
+    sleep 60
+EOF
 
   # Configure root's account to be able to easily access kubectl as well as
   # etcdctl and locksmithctl.  As we productionize this process, this code
