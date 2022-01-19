2245f76106de8542e6a1dcfcebfeee1b475b4c18
Nathan Kinkade <kinkade@measurementlab.net>
2022-01-19 16:48:01 -0700

    Install flannel CNI plugin in rootfs

--- manage-cluster/add_k8s_virtual_node.sh
+++ manage-cluster/add_k8s_virtual_node.sh
@@ -167,27 +167,32 @@ gcloud compute ssh "${GCE_NAME}" "${GCE_ARGS[@]}" <<EOF
 
   # Install CNI plugins.
   mkdir -p /opt/cni/bin
-  curl -L "https://github.com/containernetworking/plugins/releases/download/${K8S_CNI_VERSION}/cni-plugins-linux-amd64-${K8S_CNI_VERSION}.tgz" | tar -C /opt/cni/bin -xz
+  curl --location "https://github.com/containernetworking/plugins/releases/download/${K8S_CNI_VERSION}/cni-plugins-linux-amd64-${K8S_CNI_VERSION}.tgz" | tar -C /opt/cni/bin -xz
+
+  # Install the Flannel CNI plugin
+  # v0.9.1 of the official CNI plugins release stopped including flannel, so we
+  # must now install it manually from its official source.
+  curl --location "https://github.com/flannel-io/cni-plugin/releases/download/${K8S_FLANNELCNI_VERSION}/flannel-amd64" > /opt/cni/bin/flannel
 
   # Install crictl.
   mkdir -p /opt/bin
-  curl -L "https://github.com/kubernetes-incubator/cri-tools/releases/download/${K8S_CRICTL_VERSION}/crictl-${K8S_CRICTL_VERSION}-linux-amd64.tar.gz" | tar -C /opt/bin -xz
+  curl --location "https://github.com/kubernetes-incubator/cri-tools/releases/download/${K8S_CRICTL_VERSION}/crictl-${K8S_CRICTL_VERSION}-linux-amd64.tar.gz" | tar -C /opt/bin -xz
 
   # Install conntrack.
   apt install -y conntrack
 
   # Install kubeadm, kubelet and kubectl.
   cd /opt/bin
-  curl -L --remote-name-all https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64/{kubeadm,kubelet,kubectl}
+  curl --location --remote-name-all https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64/{kubeadm,kubelet,kubectl}
   chmod +x {kubeadm,kubelet,kubectl}
 
   # Install kubelet systemd service and enable it.
-  curl -sSL \
+  curl --silent --show-error --location \
     "https://raw.githubusercontent.com/kubernetes/release/${K8S_TOOLING_VERSION}/cmd/kubepkg/templates/latest/deb/kubelet/lib/systemd/system/kubelet.service" \
 	| sed "s:/usr/bin:/opt/bin:g" | sudo tee /etc/systemd/system/kubelet.service
 
   mkdir -p /etc/systemd/system/kubelet.service.d
-  curl -sSL \
+  curl --silent --show-error --location \
     "https://raw.githubusercontent.com/kubernetes/release/${K8S_TOOLING_VERSION}/cmd/kubepkg/templates/latest/deb/kubeadm/10-kubeadm.conf" \
 	| sed "s:/usr/bin:/opt/bin:g" | sudo tee /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 
