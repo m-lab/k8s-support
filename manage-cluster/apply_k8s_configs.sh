8bd79767854f69039d9bb66947c825a399aca644
Nathan Kinkade <kinkade@measurementlab.net>
2022-12-21 12:31:11 -0700

    Moves helm values override files to top level dir
    Before the helm values override files were mixed in with various jsonnet
    config files in the config/ directory. Since helm is its own thing, this
    commit creates a new top-level directory named "helm/" and moves all the
    value override files to seprate subdirectories of that directory.

--- manage-cluster/apply_k8s_configs.sh
+++ manage-cluster/apply_k8s_configs.sh
@@ -64,7 +64,7 @@ kubectl create namespace logging --dry-run=client -o json | kubectl apply -f -
 # Install ingress-nginx and set it to run on the same node as prometheus-server.
 ./linux-amd64/helm upgrade --install ingress-nginx \
   --namespace ingress-nginx \
-  --values ../config/ingress-nginx/helm-values-overrides.yaml \
+  --values ../helm/ingress-nginx/helm-values-overrides.yaml \
   ingress-nginx/ingress-nginx
 
 # Install cert-manager and configure it to use the "letsencrypt" ClusterIssuer
@@ -74,17 +74,17 @@ kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/relea
 ./linux-amd64/helm upgrade --install cert-manager \
   --namespace cert-manager \
   --version ${K8S_CERTMANAGER_VERSION} \
-  --values ../config/cert-manager/helm-values-overrides.yaml \
+  --values ../helm/cert-manager/helm-values-overrides.yaml \
   jetstack/cert-manager
 
 # Replace per-project variables in Vector's values.yaml and install Vector.
 sed -e "s|{{PROJECT}}|${PROJECT}|g" \
     -e "s|{{IMAGE}}|${K8S_VECTOR_IMAGE}|g" \
-    ../config/vector/values.yaml.template > \
-    ../config/vector/values.yaml
+    ../helm/vector/values.yaml.template > \
+    ../helm/vector/values.yaml
 
 ./linux-amd64/helm upgrade --install vector \
-  --values ../config/vector/values.yaml \
+  --values ../helm/vector/values.yaml \
   --version ${K8S_VECTOR_CHART} \
   vector/vector
 
